be66a731762940ce6a8b6fc2657275e351e51031
Randy Sargent <randy.sargent@gmail.com>
2021-02-17 15:54:45 -0500

    No longer need to setAnimate(false);  thumbnails now render from main branch

--- thumbnail-server.rb
+++ thumbnail-server.rb
@@ -118,12 +118,6 @@ class ThumbnailGenerator
       # Note: Any ajax requests or large data files may not be loaded yet.
       #       We take care of that further down when taking the actual screenshots.
       driver.navigate.to url
-      begin
-        vlog(shardno, "make_chrome: #{driver.execute_script('{canvasLayer.setAnimate(false); return timelapse.frameno}')} frames before setAnimate(false)");
-      rescue
-        next
-      end
-
       # TODO: actually wait until all the layers are loaded, and then record how long that took
       200.times do |i|
         ready = driver.execute_script("return isEarthTimeLoaded()")
